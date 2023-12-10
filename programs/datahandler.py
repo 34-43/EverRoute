47063e66fd080d9b05a7cffc88f74f48cd31bacf
34-43 <121276581+34-43@users.noreply.github.com>
2023-12-10 19:23:28 +0900

    feat: bruteforce

--- programs/datahandler.py
+++ programs/datahandler.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os as os
 import datetime as dt
+import random
 
 def make_path(dir_p,file_p):
     return os.path.join(os.getcwd(),dir_p,file_p)
@@ -70,20 +71,65 @@ class datahandler:
         if h < 10 or h >18: return -1
         new = dt.time(hour=h,minute=30)
         row_of_new = df_wd.iloc[:,2][df_wd.iloc[:,2]==str(new)].index.to_list()[0]
-        return df.loc[row_of_new,target]
+        return float(df.loc[row_of_new,target])
 
     def get_route_time(self,start,end):
         df = self.df_route
         row_of_start = df.iloc[:,1][df.iloc[:,1]==start].index.to_list()[0]
-        return df.loc[row_of_start,end]
+        return float(df.loc[row_of_start,end])
+
+    def get_ride_time(self,target):
+        return 5.0
+
+
+    # 모든 경우를 계산할 시, 26! = 403,291,461,126,605,635,584,000,000
+    # 따라서, 최대 계산 경로를 제한
+    def bruteforce(self,time=dt.datetime.today(),pos='롤링엑스트레인',max_t=180,p_list=[]):
+        time_at_enter = time
+        pos_at_enter = pos
+        max_time = max_t
+        prefer_list = p_list
+
+        result = []
+        routes = []
+        route = []
+        while(len(result) < 10):
+            route.clear()
+            acc = 0
+            
+            clock = time_at_enter
+            last_ride = pos_at_enter
+            reserver = self.key_list
+            for i in range(len(self.key_list)):
+                ride = random.choice(reserver)
+                for i in range(len(reserver)):
+                    if ride in route: ride = random.choice(reserver)
+
+                route_time = self.get_route_time(last_ride,ride)
+                clock_temp = clock + dt.timedelta(minutes=route_time)
+                await_time = self.get_await_time(ride,clock_temp)
+                ride_time = self.get_ride_time(ride)
+                
+                if await_time == -1:
+                    reserver.remove(ride)
+                    if len(reserver) < 1: break
+                    continue
+                if (acc + route_time + await_time + ride_time) > max_time:
+                    print(route, dt.timedelta(minutes=acc),ride, (acc + route_time + await_time + ride_time)-max_time)
+                    if route and route not in routes: routes.append(route); result.append({'route':route,'acc':acc})
+                    break
+                acc += route_time + await_time + ride_time
+                route.append(ride)
+                clock = time_at_enter + dt.timedelta(minutes=acc)
+                last_ride = ride
+        return result
 
 def main():
     dh = datahandler()
     dh.update()
     dh.use_route(1)
-    
-    print(dh.get_route_time('사파리월드','릴리댄스'))
-    print(dh.get_await_time('슈팅고스트'))
-    print(dh.get_await_time('피터팬',dt.datetime(2023,11,23,16,00,0)))
-
+    result = dh.bruteforce(time=dt.datetime(2023,12,10,10,30,0))
+    if result:
+        for i in range(len(result)):
+            print(result[i])
 main()
